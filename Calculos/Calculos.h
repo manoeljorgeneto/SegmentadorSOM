0218576c229c6af15ee7f11fa0aa1729e1773b48
Manoel Jorge Ribeiro Neto <manoeljorge.neto@gmail.com>
2022-05-29 10:53:09 -0300

    inaugurando github

--- Calculos/Calculos.h
+++ Calculos/Calculos.h
@@ -20,7 +20,7 @@ class Calculos {
 public:
     Calculos(); // Construtor
     virtual ~Calculos(); // Destrutor
-	
+
     static double calculaNorma(const vector<double> &a); // Módulo do vetor
     static void normalizaVetor(vector<double> *a); // Normaliza o vetor (deixando-o com norma igual a 1)
     static double calculaDistancia(const vector<double> &a, const vector<double> &b); // Distância euclidiana
--- README.md
+++ README.md
@@ -2,3 +2,13 @@
 * Autor: Manoel Jorge Ribeiro Neto
 * e-mail: manoeljorge.neto@gmail.com
 * Programa SegmentadorSOM, que utiliza o algoritmo SOM de Kohonen.
+
+
+* Para compilar:
+  * $ make clean
+  * $ make all -j\<n\>
+  * onde \<n\> é, normalmente, o número de threads de seu processador.
+
+
+* Uso:
+  * $ ./SegmentadorSOM \<argumentos\>
