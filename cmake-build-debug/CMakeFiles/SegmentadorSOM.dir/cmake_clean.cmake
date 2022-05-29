5a474d8f52bbaf9d9bc965d5aa158711605cfb8d
Manoel Jorge Ribeiro Neto <manoeljorge.neto@gmail.com>
2022-05-29 18:10:52 -0300

    inaugurando github

---
+++ cmake-build-debug/CMakeFiles/SegmentadorSOM.dir/cmake_clean.cmake
@@ -0,0 +1,23 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SegmentadorSOM.dir/Calculos/Calculos.cpp.o"
+  "CMakeFiles/SegmentadorSOM.dir/Calculos/Calculos.cpp.o.d"
+  "CMakeFiles/SegmentadorSOM.dir/Codificador/Dado.cpp.o"
+  "CMakeFiles/SegmentadorSOM.dir/Codificador/Dado.cpp.o.d"
+  "CMakeFiles/SegmentadorSOM.dir/SOM/Arranjo.cpp.o"
+  "CMakeFiles/SegmentadorSOM.dir/SOM/Arranjo.cpp.o.d"
+  "CMakeFiles/SegmentadorSOM.dir/SOM/Neuronio.cpp.o"
+  "CMakeFiles/SegmentadorSOM.dir/SOM/Neuronio.cpp.o.d"
+  "CMakeFiles/SegmentadorSOM.dir/SOM/SOM.cpp.o"
+  "CMakeFiles/SegmentadorSOM.dir/SOM/SOM.cpp.o.d"
+  "CMakeFiles/SegmentadorSOM.dir/Visualizacao/MapaContextual.cpp.o"
+  "CMakeFiles/SegmentadorSOM.dir/Visualizacao/MapaContextual.cpp.o.d"
+  "CMakeFiles/SegmentadorSOM.dir/main.cpp.o"
+  "CMakeFiles/SegmentadorSOM.dir/main.cpp.o.d"
+  "SegmentadorSOM"
+  "SegmentadorSOM.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SegmentadorSOM.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-release/CMakeFiles/SegmentadorSOM.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/manoel/CLionProjects/SegmentadorSOM/Calculos/Calculos.cpp" "CMakeFiles/SegmentadorSOM.dir/Calculos/Calculos.cpp.o" "gcc" "CMakeFiles/SegmentadorSOM.dir/Calculos/Calculos.cpp.o.d"
+  "/home/manoel/CLionProjects/SegmentadorSOM/Codificador/Dado.cpp" "CMakeFiles/SegmentadorSOM.dir/Codificador/Dado.cpp.o" "gcc" "CMakeFiles/SegmentadorSOM.dir/Codificador/Dado.cpp.o.d"
+  "/home/manoel/CLionProjects/SegmentadorSOM/SOM/Arranjo.cpp" "CMakeFiles/SegmentadorSOM.dir/SOM/Arranjo.cpp.o" "gcc" "CMakeFiles/SegmentadorSOM.dir/SOM/Arranjo.cpp.o.d"
+  "/home/manoel/CLionProjects/SegmentadorSOM/SOM/Neuronio.cpp" "CMakeFiles/SegmentadorSOM.dir/SOM/Neuronio.cpp.o" "gcc" "CMakeFiles/SegmentadorSOM.dir/SOM/Neuronio.cpp.o.d"
+  "/home/manoel/CLionProjects/SegmentadorSOM/SOM/SOM.cpp" "CMakeFiles/SegmentadorSOM.dir/SOM/SOM.cpp.o" "gcc" "CMakeFiles/SegmentadorSOM.dir/SOM/SOM.cpp.o.d"
+  "/home/manoel/CLionProjects/SegmentadorSOM/Visualizacao/MapaContextual.cpp" "CMakeFiles/SegmentadorSOM.dir/Visualizacao/MapaContextual.cpp.o" "gcc" "CMakeFiles/SegmentadorSOM.dir/Visualizacao/MapaContextual.cpp.o.d"
+  "/home/manoel/CLionProjects/SegmentadorSOM/main.cpp" "CMakeFiles/SegmentadorSOM.dir/main.cpp.o" "gcc" "CMakeFiles/SegmentadorSOM.dir/main.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
