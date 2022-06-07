e4cf58f8819b8e49ddf228edad50ee0a7c03c9f5
Manoel Jorge Ribeiro Neto <manoeljorge.neto@gmail.com>
2022-06-07 11:52:19 -0300

    Inclusão de copyright

--- Codificador/Dado.cpp
+++ Codificador/Dado.cpp
@@ -1,3 +1,25 @@
+//======================================================================================================================
+// Name        : Dado.cpp
+// Author      : Manoel Jorge Ribeiro Neto
+// e-mail      : manoeljorge.neto@gmail.com
+// Version     : v0.1.1-alpha
+// Copyright   : Copyright© 2007-2022 Manoel Jorge Ribeiro Neto
+// Description : Programa SegmentadorSOM, que utiliza o algoritmo SOM de Kohonen.
+//
+// This file is part of SegmentadorSOM.
+//
+// SegmentadorSOM is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
+// License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later
+// version.
+//
+// SegmentadorSOM is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+// warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along with SegmentadorSOM. If not, see
+// <https://www.gnu.org/licenses/>
+//======================================================================================================================
+
+
 #include "../Codificador/Dado.h"
 
 // Construtor
